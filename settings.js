cf430916d116d8b18616b6d83573cce6527e45e0
Huihui <74879576+prioprast@users.noreply.github.com>
2022-05-28 14:08:30 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -12,7 +12,7 @@ global.APIKeys = {
 }
 
 global.self_mode = false
-global.autobio = true
+global.autobio = false
 global.msg_connected = false
 global.user_register = false
 global.anti_delete = false
@@ -21,16 +21,16 @@ global.auto_welcome_msg = true
 global.auto_leave_msg = true
 global.prefix = "."
 global.language = "ind"
-global.ownerNumberr = "62887435047326"
-global.ownerbot = ['62887435047326@s.whatsapp.net','918156874290@s.whatsapp.net'] // .owner
-global.ownername = "ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×"
+global.ownerNumberr = "6285770598246"
+global.ownerbot = ['6285770598246@s.whatsapp.net','918156874290@s.whatsapp.net'] // .owner
+global.ownername = "Prio Prastyo"
 global.botname = "Alphabot"
 global.thumb = "thumbnail_1.jpg"
 global.thumbnail ="thumbnail_2_1.jpg"
 global.fakethumb ="thumbnail_1.jpg"
 global.donasi ="donasi.jpg"
-global.captionpay ="Scan untuk melakukan pembayaran"
-global.fake = "「  ALPHABOT  」\nCreated By ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×"
+global.captionpay ="Donasi\n°Dana : 6285770598246\n\n°Gopay : 6285770598246\n\n°Ovo : 6285770598246"
+global.fake = "Spam : Banned 🔕\nCall : Block 📵"
 global.alphaVersion = [2, 2143, 3] //WhatsApp web version
 
 /* Available type menu
